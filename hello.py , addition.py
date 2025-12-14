b39f2b07dd63c420118de2d11c23a77ec4ee576f
priyanka-coder-lab <priyankamhamane2@gmail.com>
2025-12-14 23:07:29 +0530

    Add multiple Python scripts for number operations

---
+++ hello.py , addition.py
@@ -0,0 +1,22 @@
+even_odd.py
+
+num = int(input("Enter number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+for_loop.py
+
+for i in range(1, 6):
+    print(i)
+
+sum_loop.py
+
+sum = 0
+
+for i in range(1, 6):
+    sum = sum + i
+
+print("Sum =", sum)
