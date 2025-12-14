f1736536ea39912080a1317b883cf18ac50e5150
priyanka-coder-lab <priyankamhamane2@gmail.com>
2025-12-14 23:16:58 +0530

    Create dict_print.py, dict_result.py, and dict_multiple.py
    Added three new Python files to handle student data and results.

---
+++ dict_print,py ,dict_result.py , dict_multiple.py
@@ -0,0 +1,39 @@
+dict_print.py
+
+student = {
+    "name": "Priyanka",
+    "marks": 85,
+    "city": "Pune"
+}
+
+for key, value in student.items():
+    print(key, ":", value)
+
+dict_result.py
+
+student = {
+    "name": "Priyanka",
+    "marks": 85
+}
+
+if student["marks"] >= 35:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
+
+dict_multiple.py
+
+student = {
+    "name": "Priyanka",
+    "marks": 85
+}
+
+if student["marks"] >= 35:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
+
+
+
+
+
