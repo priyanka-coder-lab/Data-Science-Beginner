a42ec45e2aec56e56c062d8625bee3f56fcf4527
priyanka-coder-lab <priyankamhamane2@gmail.com>
2025-12-14 23:12:55 +0530

    Add scripts for printing, summing, and finding marks

---
+++ list_print.py , list_sum_avg.py , list_max_min.py
@@ -0,0 +1,25 @@
+list_print.py
+
+marks = [60, 70, 80, 90]
+
+for m in marks:
+    print(m)
+
+list_sum_avg,py
+
+marks = [60, 70, 80, 90]
+total = sum(marks)
+avg = total / len(marks)
+
+print("Total =", total)
+print("Average =", avg)
+
+list_maxa-min.py
+
+marks = [60, 70, 80, 90]
+
+print("Highest =", max(marks))
+print("Lowest =", min(marks))
+
+
+
